c4c51dd5624c3877a81fd9a1d99a7443e76780a5
lucasgsa <lucasgsalmeida19@gmail.com>
2021-08-02 14:46:16 -0300

    update projetos

---
+++ scripts/data.js
@@ -0,0 +1,84 @@
+projetos = [
+    {
+        "nome":"Dúvidas Discord BOT",
+        "resumo": "Bot de discord, com intuito de ajudar a comunicação monitor/aluno em servidor.",
+        "detalhado":"Sério, um dos projetos simples, mas que mais me orgulho de ter feito, por simplesmente achar tão bonito seu funcionamento.\n O aluno usa '!duvida mensagem_aluno' e o monitor pode reagir pegando a dúvida para ele e pondendo responder diretamente, o que organiza bem mais o servidor, ao saber o que já foi ajudado e o que não.",
+        "adicional": "Vai lá, da uma passadinha no README, só pra ver como funciona 😉",
+        "linguagens": [
+            "JAVA"
+        ],
+        "link":"https://github.com/lucasgsa/DuvidasBot"
+    },
+    {
+        "nome":"Android MusicPlayer",
+        "resumo": "Player de música feito nativamente para android.",
+        "detalhado":
+            "Criado durante meu 2º período de universidade, com a intenção de aprender mais sobre como programar para android nativamente, ainda tem muitos bugs a resolver, mas aprendi sobre como é o funcionamento de Intents no android, BroadcastReceivers, Activitys etc. Além de começar a aprender a construir README melhores.",
+        "adicional": "Maior Aprendizado: Usar services em foreground e personalizar notificações no android.\n(Obs: Eu uso até hoje meu próprio player de música, é muito satisfatório)",
+            "linguagens": [
+            "JAVA",
+            "XML"
+        ],
+        "link":"https://github.com/lucasgsa/AndroidMusicPlayerKPNz/blob/master/README.md"
+    },
+    {
+        "nome":"Gerador de Frisos",
+        "resumo": "Script gerador dos 7 tipos de frisos",
+        "detalhado":"Fiz inicialmente como atividade extra da cadeira FMCC2 para o conteúdo de vetores. Aprendi tanto como manipular imagens de forma mais efetiva, quanto a salvar tudo isso em pastas e verificar possíveis erros. Além de conhecer mais sobre uso de comandos em script shell usando ./build.sh e ./run.sh",
+        "adicional": "Maior Aprendizado: Aprendi a manipular imagens recebidas do usuário, modificar e salvar na pasta especificada.",
+        "linguagens": [
+            "PYTHON",
+            "SHELL"
+        ],
+        "link":"https://github.com/lucasgsa/GeradorFrisos/blob/master/README.md"
+    },
+    {
+        "nome":"DiscordBot",
+        "resumo": "Bot para discord que facilita automação de mensagens sem necessidade do dono do servidor saber programar.",
+        "detalhado":"Projeto em que tento utilizar as melhores práticas de POO para tornar o bot de discord o mais modular possível. Tem intuito de facilitar a criação de bots com mensagens pré-programadas por um simples arquivo json.",
+        "adicional": "Não é necessário ter nenhum conhecimento de programação para utilizar o bot e criar suas próprias mensagens para seu servidor.",
+        "linguagens": [
+            "JAVA"
+        ],
+        "link":"https://github.com/lucasgsa/DiscordBotKPNz"
+    },
+    {
+        "nome":"2048",
+        "resumo": "Famoso jogo 2048 feito utilizando java",
+        "detalhado":"Primeira tentativa de jogo feita em java, não utilizei multithread nesse projeto por achar ele simples demais. A forma que organizei o script para fazer a movimentação das peças do 2048 deixou muito simples para reutilização POO.",
+        "adicional": "Um projeto simples mas que me fez aprender bastante sobre como fazer uma interface quase do zero, e em como é difícil centralizar objetos relativos. ",
+        "linguagens": [
+            "JAVA"
+        ],
+        "link":"https://github.com/lucasgsa/2048Java"
+    },
+    {
+        "nome":"Organizador de tarefas",
+        "resumo": "Um to-do em nuvem compartilhaveis",
+        "detalhado":"Um site organizador de tarefas, responsivo, feito utilizando html/css/javascript puro no front-end, e totalmente responsivo tanto desktop quanto mobile.\nBackend feito em java com spring boot no padrão MVC, hospedado no Heroku com banco de dados Postgresql.",
+        "adicional": "Esse projeto me ajudou a entender como trabalhar com a comunicação entre front e backend, além de também me fazer aprofundar mais em javascript.\n (OBS: Como o backend está hospedado no heroku, e tenho uma quantidade limitada de horas, ele pode não estar disponível a todo momento.)",
+        "linguagens": [
+            "HTML",
+            "CSS",
+            "JAVASCRIPT",
+            "JAVA"
+        ],
+        "link":"https://organizadordetarefas.netlify.app/"
+    },
+    {
+        "nome":"Twitter Bot TopPlayers Valorant",
+        "resumo": "Bot de twitter para mostrar todo dia os 10 melhores jogadores da região",
+        "detalhado":"Feito utilizando a API oficial disponibilizada pela Riot Games e API oficial do Twitter.",
+        "adicional": "Um projetinho bem básico mas que aprendi a usar API's de serviços grandes externos.",
+        "linguagens": [
+            "PYTHON"
+        ],
+        "link":"https://twitter.com/vlrtopbr"
+    }
+];
+
+sobre = {
+    "title":"Lucas Gabriel",
+    "subtitle":"Cursando Ciência da computação na UFCG - 2019.1",
+    "sobre": "Opa, tudo bom?\nSou fã de programação desde pequeno, com menos de 12 anos já tentava criar sites, \nclonar programas, fazer um navegador em Visual Basic ou Delphi, \ne sempre foi meu sonho seguir nessa área.\nSempre achei que programação fosse algo que me possibilitasse fazer tudo,\no que não existe ainda, pode existir, basta escrever.\nVejo isso como uma forma de ajudar, melhorar a vida das pessoas, facilitar.\nComo já disse, sempre gostei de programar, mas ainda era indeciso sobre ser isso\nque realmente queria para minha vida, foi ai que participei de uma olimpíada de programação,\nainda durante o ensino médio, e definitivamente foi algo que me marcou.\nDesde então, sempre fui muito fã de algoritmos, estrutura de dados definitivamente é\nminha parte preferida, por ter tantos desafios, e claro, precisar de muita lógica.\nProgramar me ajudou nisso, em conseguir ter um pensamento mais organizado, principalmente\nem matemática, e espero que também consiga melhorar a vida de muitas pessoas.\nLogo, comecei a criar bots, scripts, coisas básicas de programação, e quando entrei na\nuniversidade, prometi a mim mesmo que todos os períodos até o fim do curso iria fazer \nao menos um projeto pessoal, por mais simples, mas sempre aprendendo alguma tecnologia nova(mais detalhes na parte de projetos)"
+};
--- scripts/meusProjetosScript.js
+++ scripts/meusProjetosScript.js
@@ -1,32 +1,32 @@
-function switchArrow(textId){
-    var texto = document.getElementById("text_"+textId);
-    var arrow = document.getElementById("arrow_"+textId);
-    console.log(texto.style.height);
-    if (texto.style.height == "0px" | texto.style.height == ""){
-        if (window.matchMedia("only screen and (min-height:1080px) and (min-width:720px)").matches){
-            texto.style.fontSize = "40px";
-        }
-        else{
-            texto.style.fontSize = "30px";
-        }
-        texto.style.height = "initial";
-        arrow.style.transform = "rotateZ(90deg)";
-    } else {
-        texto.style.height = "0px";
-        texto.style.fontSize = "0px";
-        arrow.style.transform = "rotateZ(0deg)";
-    }
-    AOS.init();
+listaProjetosDiv = '';
+
+function generateProjetoDiv(projeto) {
+    saida = `
+    <div class="projetoDiv" data-aos="flip-down" data-aos-mirror="true" data-aos-duration="1000">
+        <div class="header_projeto">
+            <a target="_blank" href="${projeto['link']}" class="titulo_projeto">${projeto['nome']}</a>
+            <p class="resumo_projeto">${projeto['resumo']}</p>
+        </div>
+        <div class="body_projeto">
+            <p class="detalhado_projeto">${projeto['detalhado']}</p>
+            <p class="adicional_projeto">${projeto['adicional']}</p>
+            <br>
+        </div>
+    </div>
+    `;
+    return saida;
 }
 
-document.getElementById("button_musicPlayer").addEventListener("click", () => switchArrow("musicPlayer"));
+function carregarProjetos() {
+    for (projetoId in projetos){
+        listaProjetosDiv += (generateProjetoDiv(projetos[projetoId]));
+    }
+    addToDom();
+}
 
-document.getElementById("button_geradorFrisos").addEventListener("click", () => switchArrow("geradorFrisos"));
+function addToDom() {
+    projetosDiv = document.getElementById('projetos');
+    projetosDiv.innerHTML = listaProjetosDiv;
+}
 
-document.getElementById("button_2048").addEventListener("click", () => switchArrow("2048"));
-
-document.getElementById("button_discordBot").addEventListener("click", () => switchArrow("discordBot"));
-
-document.getElementById("button_classicPotatoBoss").addEventListener("click", () => switchArrow("classicPotatoBoss"));
-
-document.getElementById("button_pooPotatoBoss").addEventListener("click", () => switchArrow("pooPotatoBoss"));
+carregarProjetos();
